10c0eedf06d22d1a8ec252cdb70774fec87f5cc6
Dmitry <d@kozlov.ru>
2026-02-24 15:53:21 +0700

    Added script for bunx

--- server.ts
+++ server.ts
@@ -1,13 +1,14 @@
+#!/usr/bin/env bun
 import { randomBytes } from 'crypto'
 import { S3Server } from '.'
 
 const config = {
-  port: Number.parseInt(process.env.PORT ?? '', 10) || 9000,
-  path: process.argv[2] ?? process.env.S3_DATA_DIR ?? '.',
-  key: process.env.S3_ACCESS_KEY_ID ?? process.env.AWS_ACCESS_KEY_ID ?? randomBytes(16).toString('hex'),
-  secret: process.env.S3_SECRET_ACCESS_KEY ?? process.env.AWS_SECRET_ACCESS_KEY ?? randomBytes(16).toString('hex'),
-  sessionToken: process.env.S3_SESSION_TOKEN ?? process.env.AWS_SESSION_TOKEN ?? null,
-  maxBodyBytes: Number.parseInt(process.env.S3_MAX_BODY_BYTES ?? '0', 10) || 0,
-  maxTimeSkewSeconds: Number.parseInt(process.env.S3_MAX_TIME_SKEW_SECONDS ?? '0', 10) || 0,
+  port: Number.parseInt(Bun.env.PORT ?? '', 10) || 9000,
+  path: process.argv[2] ?? Bun.env.S3_DATA_DIR ?? '.',
+  key: Bun.env.S3_ACCESS_KEY_ID ?? Bun.env.AWS_ACCESS_KEY_ID ?? randomBytes(16).toString('hex'),
+  secret: Bun.env.S3_SECRET_ACCESS_KEY ?? Bun.env.AWS_SECRET_ACCESS_KEY ?? randomBytes(16).toString('hex'),
+  sessionToken: Bun.env.S3_SESSION_TOKEN ?? Bun.env.AWS_SESSION_TOKEN ?? null,
+  maxBodyBytes: Number.parseInt(Bun.env.S3_MAX_BODY_BYTES ?? '0', 10) || 0,
+  maxTimeSkewSeconds: Number.parseInt(Bun.env.S3_MAX_TIME_SKEW_SECONDS ?? '0', 10) || 0,
 }
 await new S3Server(config).start(true)
